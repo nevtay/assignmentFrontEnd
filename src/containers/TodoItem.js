313e8fb3fa688a77a7188f9afc17e7ab90de0928
nevtay <nevintay@gmail.com>
2020-05-03 22:27:45 +0800

    Add comments; rename variables and functions to explain their purpose more clearly

--- src/containers/TodoItem.js
+++ src/containers/TodoItem.js
@@ -7,47 +7,62 @@ import useDoubleClick from '../hooks/useDoubleClick'
 import axios from 'axios'
 
 export default function TodoItem ({ todo, todos, setTodos }) {
-  const [editing, setEditing] = useState(false)
-  const [label, setLabel] = useState(todo.label)
+  const [isEditing, setIsEditing] = useState(false)
+  const [todoText, setTodoText] = useState(todo.label)
   const [isDone, setIsDone] = useState(todo.done)
 
+  // updates todoText
   const handleSetLabel = (e) => {
-    setLabel(e.target.value)
+    setTodoText(e.target.value)
   }
 
+  // updates database whenever a todo's done state is toggled
   const handleSetIsDone = async (e) => {
     setIsDone(!isDone)
     await axios.post('https://delight-backend.herokuapp.com/update', {
       id: todo.id,
       done: !isDone,
-      label: label
+      label: todoText
     })
       .then(res => console.log(res.data))
       .catch(err => console.log(err))
   }
 
+  // if a todo is in the state of being edited, clicking outside of the todo allows user to undo all changes
   const ref = useRef()
   const wrapperRef = useRef()
   useOnClickOutside(wrapperRef, () => {
-    if (editing) {
-      setLabel(todo.label)
-      setEditing(false)
+    if (isEditing) {
+      setTodoText(todo.label)
+      setIsEditing(false)
     }
   })
 
-  const onEnter = async (e) => {
+  // handles how a todo is saved
+  // when the "enter" key is pressed, the edited todo is compared against its previous version
+  // if previous todo is the same as edited todo, hitting "enter" does not send a POST request
+  // if old todo is different from edited todo, hitting "enter" sends a POST request which updates the database
+  // regardless of the outcome, hitting the "enter" key sets the "editing" state back to false
+  const handleTodoEdit = async (e) => {
+    console.log(e.key)
     if (e.key === 'Enter') {
-      setLabel(e.target.value)
-      setEditing(false)
-      await axios.post('https://delight-backend.herokuapp.com/update', {
-        id: todo.id,
-        label: label
-      })
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err))
+      if (e.target.value === todo.label) {
+        setTodoText(todo.label)
+        setIsEditing(false)
+      } else {
+        setTodoText(e.target.value)
+        setIsEditing(false)
+        await axios.post('https://delight-backend.herokuapp.com/update', {
+          id: todo.id,
+          label: todoText
+        })
+          .then(res => console.log(res.data))
+          .catch(err => console.log(err))
+      }
     }
   }
 
+  // deletes the todo from the parent state and the database
   const onDelete = async () => {
     const todoId = todo.id
     await axios.delete(`https://delight-backend.herokuapp.com/delete/${todoId}`)
@@ -57,13 +72,15 @@ export default function TodoItem ({ todo, todos, setTodos }) {
       .catch(err => console.log(err))
   }
 
-  const handleViewClick = useDoubleClick(null, () => setEditing(true))
+  // enables double-clicking to trigger editing state on a todo
+  // this hook was already here prior to being forked
+  const handleViewClick = useDoubleClick(null, () => setIsEditing(true))
 
   return (
     <li
       ref={wrapperRef}
       onClick={handleViewClick}
-      className={`${editing ? 'editing' : ''} ${isDone ? 'completed' : ''}`}
+      className={`${isEditing ? 'editing' : ''} ${isDone ? 'completed' : ''}`}
     >
       <div className="view">
         <input
@@ -72,20 +89,20 @@ export default function TodoItem ({ todo, todos, setTodos }) {
           defaultChecked={isDone}
           onClick={(e) => handleSetIsDone(e)}
         />
-        <label>{label}</label>
+        <label>{todoText}</label>
         <button
           className="destroy"
           onClick={onDelete}
         />
       </div>
-      {editing && (
+      {isEditing && (
         <input
           ref={ref}
           type="text"
           className="edit"
-          value={label}
+          value={todoText}
           onChange={handleSetLabel}
-          onKeyPress={onEnter}
+          onKeyDown={handleTodoEdit}
         />
       )}
     </li>
